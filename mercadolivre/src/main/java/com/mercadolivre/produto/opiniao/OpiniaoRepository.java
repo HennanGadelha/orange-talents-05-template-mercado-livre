b70ddc75fa87be9dafaebec9dddad49a29719441
Hennan Gadelha <hennanhennan@gmail.com>
2021-05-29 23:19:30 -0300

    usuarioo cadastra opniao

---
+++ mercadolivre/src/main/java/com/mercadolivre/produto/opiniao/OpiniaoRepository.java
@@ -0,0 +1,7 @@
+package com.mercadolivre.produto.opiniao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OpiniaoRepository extends JpaRepository<Opiniao, Long> {
+
+}
