4e3b997cb44ab547cda99ce6d7012504880b3f44
Hennan Gadelha <hennanhennan@gmail.com>
2021-05-29 23:03:23 -0300

    Revert "adicionando opniao no produto"
    This reverts commit bd73ca957ff08c1846a91fc361f27421a655066d.

--- mercadolivre/src/main/java/com/mercadolivre/produto/opiniao/OpiniaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mercadolivre.produto.opiniao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OpiniaoRepository extends JpaRepository<Opiniao, Long> {
-
-}
